cb5cae12b6394b9b64ac0d5077342929083b0ee8
Richard Hertzog <richard.hertzog@gmail.com>
2018-01-16 22:38:13 -0800

    login form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDBRGLXJXONngbx3ZF-s5DOHwGtn6VZQuE',
+      authDomain: 'auth-3d889.firebaseapp.com',
+      databaseURL: 'https://auth-3d889.firebaseio.com',
+      projectId: 'auth-3d889',
+      storageBucket: 'auth-3d889.appspot.com',
+      messagingSenderId: '691886585063'
+    });
+  }
+
   render() {
     return (
       <View>
-        <Header headerText="Authentication" />
-        <Text>An app</Text>
+        <Header headerText='Authentication' />
+        <LoginForm />
       </View>
     );
   }
